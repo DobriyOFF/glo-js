2ce36ec54d5b9320a2ede8dac8ba0b7557428189
Sergey <segaoff@icloud.com>
2022-02-01 17:21:44 +0300

    задание 3

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 let title = "First project";
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 100;
-let rollback = getRandomArbitrary(1, 100);
+let rollback = 50;
 let fullPrice = 100000;
 let adaptive = true;
+
+
+title = prompt("Как называется ваш проект?");
+screens = prompt("Какие типы экранов нужно разработать");
+screenPrice = +prompt("Сколько будет стоить данная работа?");
+adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+fullPrice = servicePrice1 + servicePrice2 + screenPrice;
+console.log("Итоговая стоимость работ: " + fullPrice);
+let servicePercentPrice = Math.ceil(fullPrice - rollback);
+console.log("Итоговая стоимость за вычетом отката посреднику: " + servicePercentPrice);
+
+if (fullPrice >= 30000) {
+    console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    console.log("Даем скидку в 5%");
+} else if (fullPrice < 15000 && fullPrice >= 0) {
+    console.log("Скидка не предусмотрена");
+} else if (fullPrice < 0) {
+    console.log("Что то пошло не так");
+}
 
 console.log("тип title - " + typeof title);
 console.log("тип fullPrice - " + typeof fullPrice);
@@ -19,8 +43,8 @@ console.log("rollback = " + rollback);
 console.log("Процент отката посреднику за работу - "+ fullPrice * (rollback/100));
 
 
-function getRandomArbitrary(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// function getRandomArbitrary(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
