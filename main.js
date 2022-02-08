590ea2cceed71d772b133dd36db2bef786d7898a
Sergey <segaoff@icloud.com>
2022-02-08 20:53:35 +0300

    lesson8

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-let title = prompt("Как называется ваш проект?");
-let screens = prompt(
-    "Какие типы экранов нужно разработать?",
-    "Простые, Сложные, Интерактивные"
-);
-let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Сколько это будет стоить?");
-let rollback = 50;
-let allServicePrices;
-let fullPrice;
-let servicePercentPrice;
-=======
 const appData = {
     title: '',
     screens: [],
@@ -48,31 +31,12 @@ const appData = {
         
         for (let i = 0; i < 1; i++) {
             let name, price;
->>>>>>> lesson8
 
             do {
                 name = prompt("Какой тип экрана нужно разработать?", "Сложные, Простые");
             } while (!appData.isText(name));
             name = name.trim();
 
-<<<<<<< HEAD
-function getTitle(str) {
-    return (str.trim()[0].toUpperCase() + str.trim().substr(1).toLowerCase());
-}
-
-const getAllServicePrices = function (price1, price2) {
-    return price1 + price2;
-    
-}
-
-function getFullPrice(price1, price2) {
-    return price1 + price2;
-}
-
-const getServicePercentPrice = function (sum, roll) {
-    return (sum - (sum * (roll / 100)));
-}
-=======
             do {
                 price = +prompt("Сколько будет стоить данная работа?");
             }
@@ -98,7 +62,6 @@ const getServicePercentPrice = function (sum, roll) {
 
             appData.services[name + "" + i] = +price;
         }   
->>>>>>> lesson8
 
         appData.adaptive = confirm("Нужен ли адаптив на сайте?");
     },
@@ -147,18 +110,4 @@ const getServicePercentPrice = function (sum, roll) {
     },
 }
 
-<<<<<<< HEAD
-title = getTitle(title);
-allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-fullPrice = getFullPrice(screenPrice, allServicePrices);
-servicePercentPrice = getServicePercentPrice(fullPrice, rollback);
-
-console.log("Тип title - " + showTypeOf(title));
-console.log("Тип screenPrice - " + showTypeOf(screenPrice));
-console.log("Тип adaptive - " + showTypeOf(adaptive));
-
-console.log(getRollbackMessage(fullPrice));
-console.log("Стоимость верстки экранов " + screenPrice + " юани и Стоимость разработки сайта " + fullPrice + " юани");
-=======
 appData.start();
->>>>>>> lesson8
